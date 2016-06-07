3c2349b014f95f1b0bce2904c1e555f86e4e156d
Joe Grund <joe.grund@intel.com>
2016-06-07 17:05:35 -0400

    add more to the API, and some tests
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- flow-typed/jasmine.js
+++ flow-typed/jasmine.js
@@ -3,7 +3,21 @@
 declare function describe (name:string, fn:() => void):void;
 declare function fdescribe (name:string, fn:() => void):void;
 declare function beforeEach (fn:() => void):void;
+declare function afterEach (fn:() => void):void;
 declare function it (name:string, fn:() => void):void;
+declare function fit (name:string, fn:() => void):void;
 declare function expect (expected:mixed):{
-  toEqual:(actual:mixed) => void
+  toEqual:(actual:mixed) => void,
+  toBe:(actual:mixed) => void
 };
+
+declare var jasmine:Jasmine;
+declare class Jasmine {
+  createSpy(name:string): {
+    and: {
+      returnValue: (val:mixed) => Jasmine,
+      callFake: (fn:(...rest:mixed[]) => mixed) => Jasmine,
+      callThrough: () => Jasmine
+    }
+  }
+}
---
+++ test/typing.js
@@ -0,0 +1,42 @@
+//@flow
+
+describe('my fake suite', () => {
+  beforeEach(() => {
+
+  });
+
+  afterEach(() => {
+
+  });
+
+  it('should not error', () => {
+    expect('this').toBe('this');
+  });
+
+  fit('should be awesome', () => {
+    expect('this').toEqual('this');
+  });
+});
+
+//should error
+describe(1, 2);
+
+//should pass
+const spy = jasmine.createSpy('foo');
+spy.and.callFake((x) => x + 1);
+
+//should error
+const spy2 = jasmine.createSpy('foo');
+spy2.and.callFake(3);
+
+//should error
+const spy3 = jasmine.createSpy(100);
+(spy3:Object);
+
+//should pass
+const spy4 = jasmine.createSpy('bar');
+spy4.and.callThrough();
+
+//should pass
+const spy5 = jasmine.createSpy('blap');
+spy5.and.returnValue('moose');
