120d11fa23b79641b7c90df188f31f1c367c14cd
Joe Grund <grundjoseph@gmail.com>
2017-04-24 16:48:51 -0400

    add travis
    Signed-off-by: Joe Grund <grundjoseph@gmail.com>

--- run_test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# There are no tests to run, but CI needs a results file.
-echo '<testsuites><testsuite name="flow-jasmine" package="flow-jasmine" timestamp="2014-07-31" id="0" hostname="localhost" tests="0" errors="0" failures="0" skipped="0" time="0.008"></testsuite></testsuites>' > flow-jasmine-results.xml
-mv flow-jasmine-results.xml ../results
