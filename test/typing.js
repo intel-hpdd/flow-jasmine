2b8153cd865d3a811f16a520b02a88b5f174b71b
Joe Grund <grundjoseph@gmail.com>
2017-04-27 10:24:38 -0400

    Update doneOrAsyncCb to use an overload instead of an intersection
    Fixes #4.
    
    We should not have to type each describe / it / beforeEach / afterEach function when testing.
    
    Update the doneOrAsyncCb type to use an intersection instead of an union.
    
    Signed-off-by: Joe Grund <grundjoseph@gmail.com>

--- test/typing.js
+++ test/typing.js
@@ -1,19 +1,15 @@
 //@flow
 
 describe('my fake suite', () => {
-  beforeEach(() => {
-
-  });
+  beforeEach(() => {});
 
   beforeEach(async () => {
-    await new Promise((res) => {
+    await new Promise(res => {
       res(3);
     });
   });
 
-  afterEach(() => {
-
-  });
+  afterEach(() => {});
 
   it('should not error', () => {
     expect('this').toBe('this');
@@ -24,36 +20,30 @@ describe('my fake suite', () => {
   });
 
   it('should work async', async () => {
-    await new Promise((res) => res(4));
+    await new Promise(res => res(4));
   });
 });
 
-//should error
+// $ExpectError
 describe(1, 2);
 
-//should pass
 const spy = jasmine.createSpy('foo');
-spy.and.callFake((x) => x + 1);
+spy.and.callFake(x => x + 1);
 
-//should error
 const spy2 = jasmine.createSpy('foo');
 spy2.and.callFake(3);
 
-//should error
 const spy3 = jasmine.createSpy(100);
-(spy3:Object);
+(spy3: Object);
 
-//should pass
 const spy4 = jasmine.createSpy('bar');
 spy4.and.callThrough();
 
-//should pass
 const spy5 = jasmine.createSpy('blap');
 spy5.and.returnValue('moose');
 
-//should pass
 const foo = {
-  bar () {}
+  bar() {}
 };
 const spy6 = spyOn(foo, 'bar');
 spy6.and.callThrough();
