2c773cd0847f0229507fe7749a9daf590be9eb7a
Joe Grund <joe.grund@intel.com>
2017-04-27 10:57:17 -0400

    Update doneOrAsyncCb to use an overload instead of an intersection (#5)
    Fixes #4.
    
    We should not have to type each describe / it / beforeEach / afterEach function when testing.
    
    Update the doneOrAsyncCb type to use an intersection instead of an union.
    
    Signed-off-by: Joe Grund <grundjoseph@gmail.com>

--- flow-typed/jasmine.js
+++ flow-typed/jasmine.js
@@ -1,60 +1,60 @@
 // @flow
 
 type doneT = {
-  ():void,
-  fail:() => void
+  (): void,
+  fail: () => void
 };
 
-type doneCb = (done:doneT) => mixed;
-type asyncCb = () => Promise<mixed>;
-type doneOrAsyncCb = doneCb | asyncCb;
+type doneCb = doneT => mixed;
+type asyncCb = empty => Promise<mixed>;
+type doneOrAsyncCb = doneCb & asyncCb;
 
-declare function describe (name:string, fn:() => void):void;
-declare function fdescribe (name:string, fn:() => void):void;
-declare function xdescribe (name:string, fn:() => void):void;
-declare function beforeEach (fn:doneOrAsyncCb):void;
-declare function beforeEachAsync (fn:asyncCb):void;
-declare function afterEach (fn:doneOrAsyncCb):void;
-declare function it (name:string, fn:doneOrAsyncCb):void;
-declare function itAsync (name:string, fn:asyncCb):void;
-declare function fit (name:string, fn:doneOrAsyncCb):void;
-declare function xit (name:string, fn:doneOrAsyncCb):void;
-declare function expect (expected:mixed):expectation;
-declare function spyOn (fn:any, name:string):spy;
+declare function describe(name: string, fn: () => void): void;
+declare function fdescribe(name: string, fn: () => void): void;
+declare function xdescribe(name: string, fn: () => void): void;
+declare function beforeEach(fn: doneOrAsyncCb): void;
+declare function beforeEachAsync(fn: asyncCb): void;
+declare function afterEach(fn: doneOrAsyncCb): void;
+declare function it(name: string, fn: doneOrAsyncCb): void;
+declare function itAsync(name: string, fn: asyncCb): void;
+declare function fit(name: string, fn: doneOrAsyncCb): void;
+declare function xit(name: string, fn: doneOrAsyncCb): void;
+declare function expect(expected: mixed): expectation;
+declare function spyOn(fn: any, name: string): spy;
 
 type expectation = {
-  not:expectation,
-  toEqual:(actual:mixed) => void,
-  toBe:(actual:mixed) => void,
-  toBeNull:() => void,
-  toHaveBeenCalledOnceWith:(...rest:mixed[]) => void,
-  toHaveBeenCalledTwiceWith:(...rest:mixed[]) => void,
-  toHaveBeenCalledNTimesWith:(times:number, ...rest:mixed[]) => void,
-  toHaveBeenCalledWith:(...rest:mixed[]) => void,
-  toHaveBeenCalled:() => void,
-  toHaveClass:(expected:string) => void,
-  toHaveBeenCalledOnce:() => void,
-  toThrow:(err:Error | Class<Error>) => void,
-  toThrowError:(err:Error | Class<Error>, msg:string) => void
+  not: expectation,
+  toEqual: (actual: mixed) => void,
+  toBe: (actual: mixed) => void,
+  toBeNull: () => void,
+  toHaveBeenCalledOnceWith: (...rest: mixed[]) => void,
+  toHaveBeenCalledTwiceWith: (...rest: mixed[]) => void,
+  toHaveBeenCalledNTimesWith: (times: number, ...rest: mixed[]) => void,
+  toHaveBeenCalledWith: (...rest: mixed[]) => void,
+  toHaveBeenCalled: () => void,
+  toHaveClass: (expected: string) => void,
+  toHaveBeenCalledOnce: () => void,
+  toThrow: (err: Error | Class<Error>) => void,
+  toThrowError: (err: Error | Class<Error>, msg: string) => void
 };
 
-declare var jasmine:Jasmine;
+declare var jasmine: Jasmine;
 declare class Jasmine {
-  any(t:any):any;
+  any(t: any): any,
   clock(): {
-    install:() => void,
-    uninstall:() => void,
-    tick:(ms:number) => void
-  };
-  createSpy(name:string):spy
+    install: () => void,
+    uninstall: () => void,
+    tick: (ms: number) => void
+  },
+  createSpy(name: string): spy
 }
 
 type spy = {
-  (...rest:any[]):any,
-  and:{
-    returnValue:(val:mixed) => spy,
-    callFake:(fn:(...rest:mixed[]) => any) => spy,
-    callThrough:() => spy
+  (...rest: any[]): any,
+  and: {
+    returnValue: (val: mixed) => spy,
+    callFake: (fn: (...rest: mixed[]) => any) => spy,
+    callThrough: () => spy
   }
 };
 
